a55f8f1ab705b6aaba5f18948d92b3e3e06dccac
tsuwoto <tsudowoodo@gmail.com>
2017-01-24 18:42:55 -0500

    added from

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -17,6 +17,7 @@ $room = strip_tags(htmlspecialchars($_POST['room']));
 
 // Create the email and send the message
 $to = 'findmymicrowave@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$from = 'findmymicrowave@gmail.com';
 $email_subject = "New Microwave";
 $email_body = "New microwave in:\n\nBuilding: $building\n\nDetails: $room\n\n"
 // $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
